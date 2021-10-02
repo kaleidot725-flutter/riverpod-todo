5d5cc6cc4b1b4fc6a70cb3add7feaed82779c297
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2021-10-02 15:56:32 +0900

    feat: fix loading widget

--- lib/view/components/loading.dart
+++ lib/view/components/loading.dart
@@ -1,8 +1,9 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class Loading extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Text("LOADING");
+    return const Text("Loading...");
   }
 }
--- lib/view/initialize/initialize_page.dart
+++ lib/view/initialize/initialize_page.dart
@@ -1,13 +1,14 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:riverpod_todo/view/components/loading.dart';
 
 class InitializePage extends HookWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: Text("TODO")),
-        body: Center(child: Text("INITIALIZE.")),
+        body: Loading(),
     );
   }
 }
